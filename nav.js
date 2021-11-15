b665aab4b5443be1b9def8c6519c47684ad30464
Úku Parts <uku.parts@gmail.com>
2021-11-15 09:09:07 +0200

    :)

---
+++ nav.js
@@ -0,0 +1,38 @@
+function gen(ac){
+	// https://stackoverflow.com/questions/4772774/how-do-i-create-a-link-using-javascript
+	var a = document.createElement("a");
+	var text = document.createTextNode("Esileht"); 
+	a.appendChild(text);
+	a.href = "esileht.html";
+	if(ac == 0){
+		a.className = "active";
+	}
+	document.getElementById("nav").appendChild(a);
+	
+	var a = document.createElement("a");
+	var text = document.createTextNode("Kontakt"); 
+	a.appendChild(text);
+	a.href = "kontakt.html";
+	if(ac == 1){
+		a.className = "active";
+	}
+	
+	document.getElementById("nav").appendChild(a);
+	var a = document.createElement("a");
+	var text = document.createTextNode("Hinnakiri"); 
+	a.appendChild(text);
+	a.href = "hinnakiri.html";
+	if(ac == 2){
+		a.className = "active";
+	}
+	
+	document.getElementById("nav").appendChild(a);
+	var a = document.createElement("a");
+	var text = document.createTextNode("Info"); 
+	a.appendChild(text);
+	a.href = "info.html";
+	if(ac == 3){
+		a.className = "active";
+	}
+	document.getElementById("nav").appendChild(a);
+}
